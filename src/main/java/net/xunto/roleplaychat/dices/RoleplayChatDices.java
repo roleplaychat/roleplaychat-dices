875320658041f229821deee1c0f415c216cbd883
xunto <xunto.orlov@gmail.com>
2023-02-09 00:39:20 +0300

    Fix wrong interface.

--- src/main/java/net/xunto/roleplaychat/dices/RoleplayChatDices.java
+++ src/main/java/net/xunto/roleplaychat/dices/RoleplayChatDices.java
@@ -1,9 +1,10 @@
 package net.xunto.roleplaychat.dices;
 
 import net.xunto.roleplaychat.RoleplayChatCore;
+import net.xunto.roleplaychat.plugins.IPlugin;
 
 public class RoleplayChatDices implements IPlugin {
-    public static void init(RoleplayChatCore core) {
+    public void init(RoleplayChatCore core) {
         core.register(new RollCommand());
     }
 }
