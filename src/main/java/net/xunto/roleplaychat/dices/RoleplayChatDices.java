bb6d2b1cd71f7ad2ae4fd769eec0622b6730453f
xunto <xunto.orlov@gmail.com>
2023-02-14 22:58:04 +0300

    Fix roleplaychat core updated.

--- src/main/java/net/xunto/roleplaychat/dices/RoleplayChatDices.java
+++ src/main/java/net/xunto/roleplaychat/dices/RoleplayChatDices.java
@@ -2,7 +2,7 @@ package net.xunto.roleplaychat.dices;
 
 import com.google.auto.service.AutoService;
 import net.xunto.roleplaychat.RoleplayChatCore;
-import net.xunto.roleplaychat.plugins.IPlugin;
+import net.xunto.roleplaychat.pluginloader.IPlugin;
 
 @AutoService(IPlugin.class)
 public class RoleplayChatDices implements IPlugin {
