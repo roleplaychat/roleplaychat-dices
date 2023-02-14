6c72928bfde846da0d811766f5aa55122b63cf3f
xunto <xunto.orlov@gmail.com>
2023-02-14 22:53:45 +0300

    Add ServiceLoader autodiscovery.

--- src/main/java/net/xunto/roleplaychat/dices/RoleplayChatDices.java
+++ src/main/java/net/xunto/roleplaychat/dices/RoleplayChatDices.java
@@ -1,8 +1,10 @@
 package net.xunto.roleplaychat.dices;
 
+import com.google.auto.service.AutoService;
 import net.xunto.roleplaychat.RoleplayChatCore;
 import net.xunto.roleplaychat.plugins.IPlugin;
 
+@AutoService(IPlugin.class)
 public class RoleplayChatDices implements IPlugin {
     public void init(RoleplayChatCore core) {
         core.register(new RollCommand());
